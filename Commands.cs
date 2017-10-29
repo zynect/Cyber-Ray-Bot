06ba043660b8be72450737bfd86d58f7a210bda3
dalcoz <dalcoz@users.noreply.github.com>
2017-10-28 19:44:31 -0500

    Make echo work

---
+++ Commands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Dbot
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        // ~say hello -> hello
+        [Command("say")]
+        [Summary("Echos a message.")]
+        public async Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync(echo);
+        }
+    }
+}
