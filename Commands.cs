d997ff3d354ccffcda9da42c3d182285e32be53c
dalcoz <dalcoz@users.noreply.github.com>
2017-10-28 21:13:20 -0500

    Begin YouTube

--- Commands.cs
+++ Commands.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Commands;
+using Discord.WebSocket;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using Google.Apis.Services;
 
 namespace Dbot
 {
@@ -16,6 +20,41 @@ namespace Dbot
         {
             // ReplyAsync is a method on ModuleBase
             await ReplyAsync(echo);
+        }
+    }
+
+    // Create a module with the 'yt' prefix
+    [Group("yt")]
+    public class YouTube : ModuleBase<SocketCommandContext>
+    {
+        // ~sample square 20 -> 400
+        [Command("vid")]
+        [Summary("Searches for a video.")]
+        [Alias("video", "search")]
+        public async Task SquareAsync([Remainder] [Summary("The video to search for.")] string video)
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = Credentials.YouTubeApiKey,
+                ApplicationName = this.GetType().ToString()
+            });
+
+            await ReplyAsync("https://youtube.com/watch?v=" + video);
+        }
+
+        // ~sample userinfo --> foxbot#0282
+        // ~sample userinfo @Khionu --> Khionu#8708
+        // ~sample userinfo Khionu#8708 --> Khionu#8708
+        // ~sample userinfo Khionu --> Khionu#8708
+        // ~sample userinfo 96642168176807936 --> Khionu#8708
+        // ~sample whois 96642168176807936 --> Khionu#8708
+        [Command("userinfo")]
+        [Summary("Returns info about the current user, or the user parameter, if one passed.")]
+        [Alias("user", "whois")]
+        public async Task UserInfoAsync([Summary("The (optional) user to get info for")] SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
         }
     }
 }
